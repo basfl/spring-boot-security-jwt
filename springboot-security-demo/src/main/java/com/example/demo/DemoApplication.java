2a6d4ac10e9c1d9b573e150ef5751681fbb30701
unknown <jupiter85838@yahoo.com>
2020-05-02 20:14:26 -0400

    remove comments

--- springboot-security-demo/src/main/java/com/example/demo/DemoApplication.java
+++ springboot-security-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		//eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJmb28iLCJleHAiOjE1ODg0OTY1NTksImlhdCI6MTU4ODQ2MDU1OX0.31_oNfQZi1L9BvKsGhab7yh6HejRxNGn7MyXMcF0NOo
+		
 	}
 
 }
